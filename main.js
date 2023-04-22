5e3ec0b49468fe4a51b196941872dafc417fec44
phoeniXharsh <hv.tiwari178@gmail.com>
2023-04-22 17:31:58 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const container = document.getElementsByClassName("container");
+const cell = document.getElementsByClassName("cell");
+
+function makeRows(howMany){
+    for(let i=0; i<howMany; i++){
+        let row = document.createAttribute('div');
+        container.appendchild
+    }
+}
